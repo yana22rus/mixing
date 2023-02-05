9eb321b2bed3b117e0ae726c3551cb3c6039b58f
qwe <test@mail.ru>
2023-02-06 01:11:59 +0300

    add data in db use php

--- php/index.php
+++ php/index.php
@@ -2,20 +2,48 @@
 
 $uri = $_SERVER["REQUEST_URI"];
 
-if ('/' === $uri){
-    echo "root";
-}else if ("/test" === $uri){
+require_once './vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('./templates');
+$twig = new \Twig\Environment($loader);
+
+
+if ('/' === $uri) {
+
+    $template = $twig->load('index.html.twig');
+    $tmp = ['python', 'php', 'node', 'java', 'golang'];
+    echo $template->render(["tmp" => $tmp]);
+} else if ("/php" === $uri) {
+
+    $dir = 'sqlite:/home/qwe/Documents/mixing/Data.db';
+
+    if (isset($_POST['create'])){
+
+        if (isset($_POST['city'])){
+            $dbh = new PDO($dir) or die("cannot open the database");
+
+            $city = $_POST["city"];
+            $query = "INSERT INTO Data (city) VALUES ('$city')";
+            $dbh->exec($query);
+
+        }
+
+
+    }else if (isset($_POST['submit2'])) {
+        echo 123;
+    }
 
 
     require __DIR__ . "/templates/" . 'crud.htm';
 
-    $dir = 'sqlite:/home/qwe/Documents/mixing/Data.db';
-    $dbh  = new PDO($dir) or die("cannot open the database");
-    $query =  "SELECT * FROM Data";
-    foreach ($dbh->query($query) as $row)
-    {
+
+    $dbh = new PDO($dir) or die("cannot open the database");
+    $query = "SELECT * FROM Data";
+    foreach ($dbh->query($query) as $row) {
         echo "{$row[0]} $row[1]<br>";
     }
     $dbh = null;
+
+    print_r($dbh->query($query));
 
 }
